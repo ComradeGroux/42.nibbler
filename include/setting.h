44e4003272cda462ee4662a3e6db42f7c752db9a
Victor Groux <victorgroux99@gmail.com>
2026-03-12 15:50:41 +0100

    Added define in settting.h and improve --help message to use the define   - For example, added LIB1, LIB2 and LIB3 to just remplace 1 define in     case we are remplacing a graphical library

---
+++ include/setting.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ANSI-color-codes.h"
+
+#define NUMBER_OPTIONS 3
+#define WIDTH	0
+#define HEIGHT	1
+#define LIB		2
+
+#define MIN_WIDTH 480
+#define MAX_WIDTH 720
+
+#define MIN_HEIGHT 480
+#define MAX_HEIGHT 720
+
+#define LIB1 "ncurses"
+#define LIB2 "opengl"
+#define LIB3 "vulkan"
--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,24 @@
 #include <dlfcn.h>
 
 #include <iostream>
-#include "ANSI-color-codes.h"
-
 #include <string.h>
 
-#define NUMBER_OPTIONS 3
-#define WIDTH	0
-#define HEIGHT	1
-#define LIB		2
+#include "setting.h"
 
 inline static void	printUsage(void)
 {
 	std::cout << BYEL << "[USAGE]" << CRESET << " nibbler -w <WIDTH> -h <HEIGHT>" << std::endl;
 }
 
-static void	printHelp(void)
+inline static void	printHelp(void)
 {
 	std::cout << "An application to learn dynamic libraries" << std::endl << std::endl;
 	std::cout << BWHT << "USAGE:" << CRESET << " nibbler" << CRESET << " --width <WIDTH> --height <HEIGHT> [OPTIONS]" << std::endl << std::endl;
 	std::cout << BWHT << "MANDATORY FLAGS:" << CRESET << std::endl;
-	std::cout << "  -w <WIDTH>,  --width <WIDTH>                   Width of the window in pixels" << std::endl;
-	std::cout << "  -h <HEIGHT>, --height <HEIGHT>                 Height of the window in pixels" << std::endl << std::endl;
+	std::cout << "  -w <WIDTH>,  --width <WIDTH>                   Width of the window in pixels. Must be between " << BWHT << MIN_WIDTH << CRESET << " and " << BWHT << MAX_WIDTH << CRESET << std::endl;
+	std::cout << "  -h <HEIGHT>, --height <HEIGHT>                 Height of the window in pixels. Must be between " << BWHT << MIN_HEIGHT << CRESET << " and " << BWHT << MAX_HEIGHT << CRESET<< std::endl << std::endl;
 	std::cout << BWHT << "OPTIONNAL FLAGS:" << CRESET << std::endl;
-	std::cout << "               --lib [" << UWHT << "ncurses" << CRESET << ", opengl, vulkan]   Library to use at launched" << std::endl;
+	std::cout << "  -l,          --lib [" << UWHT << LIB1 << CRESET << ", " << LIB2 << ", " << LIB3 << "]   Library to use at launched" << std::endl;
 	std::cout << "               --help                            Print this help message" << std::endl;
 }
 
@@ -50,13 +45,14 @@ static int	parseArguments(int argc, char **argv, char **options)
 		if (strcmp(argv[i], "--help") == 0)
 		{
 			printHelp();
-			return 1;
+			freeOptions(options);
+			exit(EXIT_SUCCESS);
 		}
 		else if ((strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--width") == 0) && argv[i + 1] && argv[i + 1][0] != '-')
 			options[WIDTH] = strdup(argv[++i]);
 		else if ((strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--height") == 0) && argv[i + 1] && argv[i + 1][0] != '-')
 			options[HEIGHT] = strdup(argv[++i]);
-		else if (strcmp(argv[i], "-lib") == 0 && argv[i + 1] && argv[i + 1][0] != '-')
+		else if ((strcmp(argv[i], "-l") == 0 || strcmp(argv[i], "--lib") == 0) && argv[i + 1] && argv[i + 1][0] != '-')
 			options[LIB] = strdup(argv[++i]);
 		else
 		{
@@ -79,7 +75,35 @@ static int	parseArguments(int argc, char **argv, char **options)
 		return -1;
 	}
 	else if (options[LIB] == NULL)
-		options[LIB] = strdup("ncurses");
+		options[LIB] = strdup(LIB1);
+
+	return 0;
+}
+
+static int	processArguments(char **options, int &width, int &height)
+{
+	width = std::atoi(options[WIDTH]);
+	height = std::atoi(options[HEIGHT]);
+
+	if (width < MIN_WIDTH || width > MAX_WIDTH)
+	{
+		std::cerr << BRED << "[ERROR]" << CRESET << " Wrong width value, must be included between " <<
+					 BWHT << MIN_WIDTH << CRESET << " and " << BWHT << MAX_WIDTH << CRESET << std::endl;
+		return -1;
+	}
+	if (height < MIN_HEIGHT || height > MAX_HEIGHT)
+	{
+		std::cerr << BRED << "[ERROR]" << CRESET << " Wrong height value, must be included between " <<
+					 BWHT << MIN_HEIGHT << CRESET << " and " << BWHT << MAX_HEIGHT << CRESET << std::endl;
+		return -1;
+	}
+
+	if (strcmp(options[LIB], LIB1) != 0 && strcmp(options[LIB], LIB2) != 0 && strcmp(options[LIB], LIB3) != 0)
+	{
+		std::cerr << BRED << "[ERROR]" << CRESET << " The selectionned library is not supported. Available choices: " <<
+					 BWHT << LIB1 << CRESET << ", " << BWHT << LIB2 << CRESET << " or " << BWHT << LIB3 << CRESET << std::endl;
+		return -1;
+	}
 
 	return 0;
 }
@@ -87,7 +111,10 @@ static int	parseArguments(int argc, char **argv, char **options)
 int	main(int argc, char** argv)
 {
 	char	*options[NUMBER_OPTIONS] = { NULL };
-	if (parseArguments(argc, argv, options) == -1)
+	int	width = 0;
+	int	height = 0;
+
+	if (parseArguments(argc, argv, options) == -1 || processArguments(options, width, height) == -1)
 	{
 		freeOptions(options);
 		return EXIT_FAILURE;
