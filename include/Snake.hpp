82e7673b05da995ab5610c058b6596f71e206aeb
Victor Groux <victorgroux99@gmail.com>
2026-03-16 16:51:08 +0100

    Game logic seems to be finished and OK

--- include/Snake.hpp
+++ include/Snake.hpp
@@ -3,13 +3,26 @@
 #include "Level.hpp"
 #include "LibLoader.hpp"
 
+#include <queue>
+
 class Snake
 {
 	private:
+		typedef enum	e_facing {
+			NORTH,
+			EAST,
+			SOUTH,
+			WEST
+		} t_facing;
+
 		Level		_level;
 		LibLoader	_loader;
 
+		t_facing						_direction;
+		std::queue<std::pair<int, int>>	_pos;
+
 		void	_handleInput(e_keycode input);
+		int		_move(void);
 
 	public:
 		Snake(int width, int height, char *lib_name);
--- src/Snake.cpp
+++ src/Snake.cpp
@@ -1,15 +1,42 @@
 #include "Snake.hpp"
-
 #include "setting.h"
+
+#include <unistd.h>
+#include <iostream>
 
 Snake::Snake(int width, int height, char *lib_name) : _level(width, height)
 {
 	_loader.load(lib_name);
+	if (width > height)
+		_direction = EAST;
+	else
+		_direction = SOUTH;
+
+	std::pair<int, int>	p;
+	int	x = width / 2;
+	int	y = height / 2;
+	for (unsigned int i = 4; i > 0; i--)
+	{
+		if (width > height)
+		{
+			p.first = x - i;
+			p.second = y;
+		}
+		else
+		{
+			p.first = x;
+			p.second = y - i;
+		}
+		_level.setCell(p.first, p.second, Level::E_SNAKE);
+		_pos.push(p);
+	}
 }
 
 Snake::Snake(const Snake& src) : _level(src._level)
 {
 	_loader = src._loader;
+	_direction = src._direction;
+	_pos = src._pos;
 }
 
 Snake&	Snake::operator=(const Snake& src)
@@ -19,6 +46,8 @@ Snake&	Snake::operator=(const Snake& src)
 
 	_loader = src._loader;
 	_level = src._level;
+	_direction = src._direction;
+	_pos = src._pos;
 
 	return *this;
 }
@@ -40,19 +69,86 @@ void	Snake::_handleInput(e_keycode input)
 		case E_KEY_THREE:
 			_loader.load(LIB3);
 			break;
+		case E_KEY_UP:
+			if (_direction == NORTH || _direction == SOUTH)
+				break;
+			_direction = NORTH;
+			break;
+		case E_KEY_RIGHT:
+			if (_direction == EAST || _direction == WEST)
+				break;
+			_direction = EAST;
+			break;
+		case E_KEY_DOWN:
+			if (_direction == SOUTH || _direction == NORTH)
+				break;
+			_direction = SOUTH;
+			break;
+		case E_KEY_LEFT:
+			if (_direction == WEST || _direction == EAST)
+				break;
+			_direction = WEST;
+			break;
 		default:
 			break;
 	}
 }
 
+int	Snake::_move(void)
+{
+	std::pair<int, int>	p = _pos.back();
+	switch (_direction)
+	{
+		case NORTH:
+			p.second -= 1;
+			break;
+		case EAST:
+			p.first += 1;
+			break;
+		case SOUTH:
+			p.second += 1;
+			break;
+		case WEST:
+			p.first -= 1;
+			break;
+		default:
+			break;
+	}
+	Level::t_cell	cell = _level.getCell(p.first, p.second);
+	if (cell == Level::E_WALL || cell == Level::E_SNAKE)
+		return -1;
+	else if (cell == Level::E_FOOD)
+	{
+		_level.setCell(p.first, p.second, Level::E_SNAKE);
+		_pos.push(p);
+		_level.generateFood();
+		return 1;
+	}
+	_level.setCell(p.first, p.second, Level::E_SNAKE);
+	_pos.push(p);
+
+	p = _pos.front();
+	_pos.pop();
+	_level.setCell(p.first, p.second, Level::E_EMPTY);
+	return 0;
+}
+
 void	Snake::start(void)
 {
 	_level.generateFood();
-	while (_loader.get()->getInput() != E_KEY_ESC)
+	t_keycode	input = E_KEY_UP;
+	while (input != E_KEY_ESC)
 	{
-		_handleInput(_loader.get()->getInput());
-
-		_loader.get()->clear();
 		_loader.get()->render(_level);
+
+		input = _loader.get()->getInput();
+		_handleInput(input);
+
+		if (_move() == -1)
+		{
+			_loader.unload();
+			std::cout << BRED << "You died" << CRESET << std::endl;
+			break;
+		}
 	}
 }
